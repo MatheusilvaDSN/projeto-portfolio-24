09bd739d4819c3e5db6ff001ead50878333b5bec
Matheus Silva <silva.matheus.dsn@gmail.com>
2024-04-02 18:25:49 -0300

    correcao js

--- pages/subPages.js
+++ pages/subPages.js
@@ -1,10 +1,10 @@
 function clickMenu() {
     if (menu__mb.style.display == 'block') {
         menu__mb.style.display = 'none'
-        btn__humb.setAttribute("src", "./assets/menu.svg")
+        btn__humb.setAttribute("src", "../../../assets/menu.svg")
     } else {
         menu__mb.style.display = 'block'
-        btn__humb.setAttribute("src", "./assets/close.svg")
+        btn__humb.setAttribute("src", "../../../assets/close.svg")
     }
 }
 
