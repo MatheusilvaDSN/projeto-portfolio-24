a84939ccfb529ed2c4fbb25a44544d7ced3a8e99
Matheus Silva <silva.matheus.dsn@gmail.com>
2024-04-02 18:11:25 -0300

    mb correcao

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-function clickMenu() {
+function mbIndex() {
     if (menu__mb.style.display == 'block') {
         menu__mb.style.display = 'none'
         btn__humb.setAttribute("src", "./assets/menu.svg")
-        btn__humb.setAttribute("src", "../../assets/menu.svg")
-        btn__humb.setAttribute("src", "../../../assets/menu.svg")
     } else {
         menu__mb.style.display = 'block'
         btn__humb.setAttribute("src", "./assets/close.svg")
+    }
+}
+
+function mbPage() {
+    if (menu__mb.style.display == 'block') {
+        menu__mb.style.display = 'none'
+        btn__humb.setAttribute("src", "../../assets/menu.svg")
+    } else {
+        menu__mb.style.display = 'block'
         btn__humb.setAttribute("src", "../../assets/close.svg")
+    }
+}
+
+function mbSub() {
+    if (menu__mb.style.display == 'block') {
+        menu__mb.style.display = 'none'
+        btn__humb.setAttribute("src", "../../../assets/menu.svg")
+    } else {
+        menu__mb.style.display = 'block'
         btn__humb.setAttribute("src", "../../../assets/close.svg")
     }
 }
