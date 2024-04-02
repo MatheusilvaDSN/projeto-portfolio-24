3894d5de1581ea3444e2731191592c44b05ca48d
Matheus Silva <silva.matheus.dsn@gmail.com>
2024-04-02 17:56:43 -0300

    menu__mb

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function hv() {
-    icon__arrow.setAttribute("src", "./assets/arrow_outward.svg")
+function clickMenu() {
+    if (menu__mb.style.display == 'block') {
+        menu__mb.style.display = 'none'
+        btn__humb.setAttribute("src", "./assets/menu.svg")
+        btn__humb.setAttribute("src", "../../assets/menu.svg")
+        btn__humb.setAttribute("src", "../../../assets/menu.svg")
+    } else {
+        menu__mb.style.display = 'block'
+        btn__humb.setAttribute("src", "./assets/close.svg")
+        btn__humb.setAttribute("src", "../../assets/close.svg")
+        btn__humb.setAttribute("src", "../../../assets/close.svg")
+    }
 }
